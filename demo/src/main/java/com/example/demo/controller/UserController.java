b0b047ac23dc8265876d7c2243f9d1002398a4ed
Daisuke-hub <daisuke.general@gmail.com>
2020-05-03 07:22:40 +0000

    [add] user registration

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.data.repository.UserRepository;
 import java.util.List;
 import com.example.demo.data.entity.User;
 import org.springframework.ui.Model;
+import com.example.demo.form.UserForm;
 
 
 @Controller
@@ -15,6 +17,32 @@ public class UserController {
   // userRepositoryを使えるようにする
   @Autowired
   private UserRepository userRepository;
+
+  @GetMapping("/newuser")
+  public String getNewUser(Model model){
+
+    UserForm userForm = new UserForm();
+    model.addAttribute("userForm", userForm);
+    return "newuser";
+  }
+
+  // ユーザの新規登録をする
+  @PostMapping("/newuser")
+  // HTMLのimputタグで入力した値をUserFormとして受け取る
+  public String registerUser(UserForm userForm){
+
+    // 受け取った値をデータベースに保存するためEntiryにセットする
+    // lombokの@Dataはsetter/getterメソッドで呼び出せるっぽい
+    User user = new User();
+    user.setName(userForm.getName());
+    user.setEmail(userForm.getEmail());
+    // 受け取った値をデータベースに保存する
+    // save()はUserRepositoryのメソッドらしい=>save()だけでデータベースに登録できる
+    userRepository.save(user);
+
+    // usersテンプレートを表示する(redirectを記述する理由は不明..いらないのでは？)
+    return "redirect:/users";
+  }
 
   // httpメソッドのGETメソッドのみ受け付けて/usersが指定されたら実行するっぽい
   // @RequestMapping(path = "/user", method = RequestMethod.GET)の省略形
